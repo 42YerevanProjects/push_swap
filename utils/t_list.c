b7f359c23b9cc9ca596d5b3c15724f0eb8407c7c
Nathanael <sargishovsepyan2001@gmail.com>
2021-07-24 16:22:44 -0400

    The utils functions that help to deal with Linked Lists

---
+++ utils/t_list.c
@@ -0,0 +1,90 @@
+#include "../includes/push_swap.h"
+
+// Adappted version of Libft functions that deal with Linked Lists
+
+// Creates new node and returns the pointer of it
+t_list	*ft_lstnew(int value)
+{
+        t_list *new;
+
+        new = (t_list *) malloc(sizeof(*new));
+        if (!new)
+                return (NULL);
+        new->value = value;
+        new->next = NULL;
+        return (new);
+}
+
+// Adds the specified node to a stack (list) making it the head
+
+void	ft_lstadd_front(t_list **stack, t_list *new)
+{
+        new->next = *stack;
+        *stack = new;
+}
+
+// Returns the last node of a list 
+
+t_list	*ft_lstlast(t_list *head)
+{
+	t_list	*tmp;
+
+	tmp = head;
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+		if (tmp->next == NULL)
+			return (tmp);
+	}
+	return (tmp);
+}
+
+// Adds the specified node to a stack (list) making it the last node
+
+void	ft_lstadd_back(t_list **stack, t_list *new)
+{
+	t_list  *n;
+
+	if (*stack)
+	{
+		n = ft_lstlast(*stack);
+		n->next = new;
+		new->next = NULL;
+	}
+	else
+	{
+		*stack = new;
+		(*stack)->next = NULL;
+	}
+}
+
+// Returns the size of the Linked List
+
+int	ft_lstsize(t_list *head)
+{
+	size_t  i;
+	t_list	*tmp;
+
+	tmp = head;
+	i = 0;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
+
+// Prints the Linked List
+
+void printList(t_list *head)
+{
+    t_list      *tmp;
+
+	tmp = head;
+    while(tmp != NULL)
+	{
+        printf("%d\n", tmp->value);
+        tmp = tmp->next;
+    }
+}
