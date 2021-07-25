0e8def5e2c4c7522db0b94a9263637b1218d0f03
Nathanael <sargishovsepyan2001@gmail.com>
2021-07-25 11:10:29 -0400

    Theinstruction functions for data manipulation required by the project

--- src/instructions.c
+++ src/instructions.c
@@ -2,7 +2,7 @@
 
 // Swaps first two elements of a stack | sa and sb
 
-void	swap(t_list **stack)
+static void	swap(t_list **stack)
 {
 	t_list	*head;
 	t_list	*next;
@@ -19,15 +19,28 @@ void	swap(t_list **stack)
 	next->value = tmp_val;
 }
 
+void	sa(t_list **stack_a)
+{
+	swap(stack_a);
+	ft_putendl_fd("sa", 1);
+}
+
+void	sb(t_list **stack_b)
+{
+	swap(stack_b);
+	ft_putendl_fd("sb", 1);
+}
+
 void	ss(t_list **stack_a, t_list **stack_b)
 {
 	swap(stack_a);
 	swap(stack_b);
+	ft_putendl_fd("ss", 1);
 }
 
 // Takes the first element of one stack and puts it at the top of another | pa and pb
 
-void	push(t_list **stack_to, t_list **stack_from)
+static void	push(t_list **stack_to, t_list **stack_from)
 {
 	t_list	*tmp;
 	t_list	*head_to;
@@ -51,4 +64,106 @@ void	push(t_list **stack_to, t_list **stack_from)
 		tmp->next = head_to;
 		*stack_to = tmp;
 	}
+}
+
+void	pa(t_list **stack_a, t_list **stack_b)
+{
+	push(stack_a, stack_b);
+	ft_putendl_fd("pa", 1);
+}
+
+void	pb(t_list **stack_a, t_list **stack_b)
+{
+	push(stack_b, stack_a);
+	ft_putendl_fd("pb", 1);
+}
+
+// Shift up all elements of a stack by 1. The first element becomes the last one | ra and rb
+
+static void	rotate(t_list **stack)
+{
+	t_list	*head;
+	int		tmp;
+
+	head = *stack;
+	tmp = head->value;
+
+	while (head)
+	{
+		if (head->next == NULL)
+		{
+			head->value = tmp;
+			return ;
+		}
+		head->value = head->next->value;
+		head = head->next;
+	}
+}
+
+void	ra(t_list **stack_a)
+{
+	rotate(stack_a);
+	ft_putendl_fd("ra", 1);
+}
+
+void	rb(t_list **stack_b)
+{
+	rotate(stack_b);
+	ft_putendl_fd("rb", 1);
+}
+
+void	rr(t_list **stack_a, t_list **stack_b)
+{
+	rotate(stack_a);
+	rotate(stack_b);
+	ft_putendl_fd("rr", 1);
+}
+
+
+// Shifts down all elements of a stack by 1. The last element becomes the first one | rra and rrb
+
+static void	reverseRotate(t_list **stack)
+{
+	t_list	*head;
+	int		*values;
+	int		i;
+
+	head = *stack;
+	values = (int *)malloc(ft_lstsize(head) * sizeof(int));
+	values[0] = ft_lstlast(head)->value;
+	i = 1;
+	while (head && head->next)
+	{
+		values[i] = head->value;
+		head = head->next;
+		i++;
+	}
+	head = *stack;
+	i = 0;
+	while (head)
+	{
+		head->value = values[i];
+		head = head->next;
+		i++;
+	}
+	free(values);
+}
+
+void	rra(t_list **stack_a)
+{
+	reverseRotate(stack_a);
+	ft_putendl_fd("rra", 1);
+}
+
+void	rrb(t_list **stack_b)
+{
+	reverseRotate(stack_b);
+	ft_putendl_fd("rrb", 1);
+}
+
+void	rrr(t_list **stack_a, t_list **stack_b)
+{
+	reverseRotate(stack_a);
+	reverseRotate(stack_b);
+	ft_putendl_fd("rrr", 1);
 }
