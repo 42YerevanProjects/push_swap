e77e43e9fb3fe618451e9652b2f71c5185aebc1b
Nathanael <sargishovsepyan2001@gmail.com>
2021-07-25 11:10:57 -0400

    The header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -14,7 +14,7 @@ typedef struct	s_list
         struct s_list *next;
 }				t_list;
 
-// Utils functions
+// Util functions
 
 t_list	*ft_lstnew(int value);
 t_list	*ft_lstlast(t_list *head);
@@ -28,9 +28,17 @@ void	ft_error(char *msg);
 
 // Instruction functions
 
-void	swap(t_list **stack);
+void	sa(t_list **stack_a);
+void	sb(t_list **stack_b);
 void	ss(t_list **stack_a, t_list **stack_b);
-void	push(t_list **stack_to, t_list **stack_from);
+void	pa(t_list **stack_a, t_list **stack_b);
+void	pb(t_list **stack_b, t_list **stack_a);
+void	ra(t_list **stack_a);
+void	rb(t_list **stack_b);
+void	rr(t_list **stack_a, t_list **stack_b);
+void	rra(t_list **stack_a);
+void	rrb(t_list **stack_b);
+void	rrr(t_list **stack_a, t_list **stack_b);
 
 #endif
 
