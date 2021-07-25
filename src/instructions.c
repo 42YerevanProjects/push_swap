8fc46feae2df381c3741b0435a98f52356fc3d8b
Nathanael <sargishovsepyan2001@gmail.com>
2021-07-25 15:56:04 -0400

    The instruction functions required by the project to manipulate Linked Lists

--- src/instructions.c
+++ src/instructions.c
@@ -2,54 +2,62 @@
 
 // Swaps first two elements of a stack | sa and sb
 
-static void	swap(t_list **stack)
+static int	swap(t_list **stack)
 {
 	t_list	*head;
 	t_list	*next;
 	int		tmp_val;
 
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
 	head = *stack;
 	next = head->next;
-	if (ft_lstsize(head) < 2)
-		return ;
 	if (!head && !next)
 		ft_error("Error occured while swapping!");
 	tmp_val = head->value;
 	head->value = next->value;
 	next->value = tmp_val;
+	return (0);
 }
 
-void	sa(t_list **stack_a)
+int	sa(t_list **stack_a)
 {
-	swap(stack_a);
+	if (swap(stack_a) == -1)
+		return (-1);
 	ft_putendl_fd("sa", 1);
+	return (0);
 }
 
-void	sb(t_list **stack_b)
+int	sb(t_list **stack_b)
 {
-	swap(stack_b);
+	if(swap(stack_b) == -1)
+		return (-1);
 	ft_putendl_fd("sb", 1);
+	return (0);
 }
 
-void	ss(t_list **stack_a, t_list **stack_b)
-{
+int	ss(t_list **stack_a, t_list **stack_b)
+{	
+	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+		return (-1);
 	swap(stack_a);
 	swap(stack_b);
 	ft_putendl_fd("ss", 1);
+	return (0);
 }
 
 // Takes the first element of one stack and puts it at the top of another | pa and pb
 
-static void	push(t_list **stack_to, t_list **stack_from)
+static int	push(t_list **stack_to, t_list **stack_from)
 {
 	t_list	*tmp;
 	t_list	*head_to;
 	t_list	*head_from;
 
+	if (ft_lstsize(*stack_from) == 0)
+		return (-1);
 	head_to = *stack_to;
 	head_from = *stack_from;
-	if (ft_lstsize(head_from) == 0)
-		return ;
 	tmp = head_from;
 	head_from = head_from->next;
 	*stack_from = head_from;
@@ -64,61 +72,78 @@ static void	push(t_list **stack_to, t_list **stack_from)
 		tmp->next = head_to;
 		*stack_to = tmp;
 	}
+	return (0);
 }
 
-void	pa(t_list **stack_a, t_list **stack_b)
+int	pa(t_list **stack_a, t_list **stack_b)
 {
-	push(stack_a, stack_b);
+	if (push(stack_a, stack_b) == -1)
+		return (-1);
 	ft_putendl_fd("pa", 1);
+	return (0);
 }
 
-void	pb(t_list **stack_a, t_list **stack_b)
+int	pb(t_list **stack_a, t_list **stack_b)
 {
-	push(stack_b, stack_a);
+	if (push(stack_b, stack_a) == -1)
+		return (-1);
 	ft_putendl_fd("pb", 1);
+	return (0);
 }
 
 // Shift up all elements of a stack by 1. The first element becomes the last one | ra and rb
 
-static void	rotate(t_list **stack)
+static int	rotate(t_list **stack)
 {
 	t_list	*head;
 	t_list	*tail;
 
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
 	head = *stack;
 	tail = ft_lstlast(head);
 	*stack = head->next;
 	head->next = NULL;
 	tail->next = head;
+	return (0);
 }
 
-void	ra(t_list **stack_a)
+int	ra(t_list **stack_a)
 {
-	rotate(stack_a);
+	if (rotate(stack_a) == -1)
+		return (-1);
 	ft_putendl_fd("ra", 1);
+	return (0);
 }
 
-void	rb(t_list **stack_b)
+int	rb(t_list **stack_b)
 {
-	rotate(stack_b);
+	if (rotate(stack_b) == -1)
+		return (-1);
 	ft_putendl_fd("rb", 1);
+	return (0);
 }
 
-void	rr(t_list **stack_a, t_list **stack_b)
+int	rr(t_list **stack_a, t_list **stack_b)
 {
+	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+		return (-1);
 	rotate(stack_a);
 	rotate(stack_b);
 	ft_putendl_fd("rr", 1);
+	return (0);
 }
 
 
 // Shifts down all elements of a stack by 1. The last element becomes the first one | rra and rrb
 
-static void	reverseRotate(t_list **stack)
+static int	reverseRotate(t_list **stack)
 {
 	t_list	*head;
 	t_list	*tail;
 
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
 	head = *stack;
 	tail = ft_lstlast(head);
 	while (head)
@@ -132,24 +157,32 @@ static void	reverseRotate(t_list **stack)
 	}
 	tail->next = *stack;
 	*stack = tail;
+	return (0);
 }
 
-void	rra(t_list **stack_a)
+int	rra(t_list **stack_a)
 {
-	reverseRotate(stack_a);
+	if (reverseRotate(stack_a) == -1)
+		return (-1);
 	ft_putendl_fd("rra", 1);
+	return (0);
 }
 
-void	rrb(t_list **stack_b)
+int	rrb(t_list **stack_b)
 {
-	reverseRotate(stack_b);
+	if (reverseRotate(stack_b) == -1)
+		return (-1);
 	ft_putendl_fd("rrb", 1);
+	return (0);
 }
 
-void	rrr(t_list **stack_a, t_list **stack_b)
+int	rrr(t_list **stack_a, t_list **stack_b)
 {
+	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+		return (-1);
 	reverseRotate(stack_a);
 	reverseRotate(stack_b);
 	ft_putendl_fd("rrr", 1);
+	return (0);
 }
 
