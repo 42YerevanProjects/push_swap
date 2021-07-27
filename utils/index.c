9caf03b4880474a8f939317db569f9d9537f701b
Nathanael <sargishovsepyan2001@gmail.com>
2021-07-27 14:48:55 -0400

    Gives indexes to each element of the stack

---
+++ utils/index.c
@@ -0,0 +1,36 @@
+#include "../includes/push_swap.h"
+
+static t_list	*get_next_min(t_list **stack)
+{
+	t_list	*head;
+	t_list	*min;
+	int		has_min;
+
+	min = NULL;
+	has_min = 0;
+	head = *stack;
+	if (head)
+	{
+		while (head)
+		{
+			if ((head->index == -1) && (!has_min || head->value < min->value))
+			{
+				min = head;
+				has_min = 1;
+			}
+			head = head->next;
+		}
+	}
+	return (min);
+}
+
+void	index_stack(t_list **stack)
+{
+	t_list	*head;
+	int		index;
+
+	index = 0;
+	while((head = get_next_min(stack)))
+		head->index = index++;
+}
+
