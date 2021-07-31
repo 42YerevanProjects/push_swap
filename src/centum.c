6681ce57ce74947979c742847c8139754bda3261
Sargis Hovsepyan <shovsepy@c1r5s1.42yerevan.am>
2021-07-31 06:16:58 +0400

    The function that sorts 100 numbers

--- src/centum.c
+++ src/centum.c
@@ -1,51 +1,62 @@
 #include "../includes/push_swap.h"
-#include <stdio.h>
 
-void	cent_sort(t_list **stack_a, t_list **stack_b)
+static void	treat_stack_a(t_list **stack_a, t_list **stack_b, int chunk)
 {
 	t_list	*head;
-	int		chunk;
-	int		i;
 	int		distance;
 
 	head = *stack_a;
+	while (ft_lstsize(*stack_a) != 0)
+	{
+		if (head->index < chunk)
+		{
+			distance = get_distance(stack_a, head->index);
+			make_top(stack_a, distance);
+			pb(stack_a, stack_b);
+			head = *stack_a;
+		}
+		else if (ft_lstsize(*stack_a) > (100 - chunk))
+			head = head->next;
+		else
+			break ;
+	}
+}
+
+static void	treat_stack_b(t_list **stack_a, t_list **stack_b, int i)
+{
+	t_list	*head;
+	int		distance;
+
+	head = *stack_b;
+	while (ft_lstsize(*stack_b) != 0)
+	{
+		if (head->index == i)
+		{
+			distance = get_distance(stack_b, i);
+			make_top(stack_b, distance);
+			pa(stack_a, stack_b);
+			break ;
+		}
+		else
+			head = head->next;
+	}
+}
+
+void	cent_sort(t_list **stack_a, t_list **stack_b)
+{
+	int		i;
+	int		chunk;
+
 	chunk = 20;
 	i = 5;
-	while (i > 0)
+	while (i-- > 0)
 	{
-		while (ft_lstsize(*stack_a) != 0)
-		{
-			if (head->index < chunk)
-			{
-				distance = get_distance(stack_a, head->index);
-				make_top(stack_a, distance);
-				pb(stack_a, stack_b);
-				head = *stack_a;
-			}
-			else if (ft_lstsize(*stack_a) > (100 - chunk))
-				head = head->next;
-			else
-				break ;
-		}
-		chunk = chunk + 20;
-		i--;
+		treat_stack_a(stack_a, stack_b, chunk);
+		chunk = 2 * chunk;
 	}
+	if (!is_sorted(stack_a))
+		sa(stack_a);
 	i = 99;
 	while (i >= 0)
-	{
-		head = *stack_b;
-		while (ft_lstsize(*stack_b) != 0)
-		{
-			if (head->index == i)
-			{
-				distance = get_distance(stack_b, i);
-				make_top(stack_b, distance);
-				pa(stack_a, stack_b);
-				break ;
-			}
-			else
-				head = head->next;
-		}
-		i--;
-	}
+		treat_stack_b(stack_a, stack_b, i--);
 }
