fc5d9d92430743029f5498453e7ed772e34a8a38
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-01 13:13:29 -0400

    Maked it optimized: it works with minimums rather than 0s

--- src/simple.c
+++ src/simple.c
@@ -1,5 +1,6 @@
 #include "../includes/push_swap.h"
 
+/*
 static void	decrement_index(t_list **stack)
 {
 	t_list	*head;
@@ -10,34 +11,57 @@ static void	decrement_index(t_list **stack)
 		head->index--;
 		head = head->next;
 	}
+} */
+
+static int	get_min(t_list **stack, int val)
+{
+	t_list	*head;
+	int		min;
+
+	head = *stack;
+	min = head->index;
+	while (head->next)
+	{
+		head = head->next;
+		if ((head->index < min) && head->index != val)
+			min = head->index;
+	}
+	return (min);
 }
 
 void	sort_3(t_list **stack_a)
 {
 	t_list	*head;
+	int		min;
+	int		next_min;
 
 	head = *stack_a;
+	min = get_min(stack_a, -1);
+	next_min = get_min(stack_a, min);
 	if (is_sorted(stack_a))
 		return ;
-	if (head->index == 0 && head->next->index != 1)
+	if (head->index == min && head->next->index != next_min)
 	{
 		ra(stack_a);
 		sa(stack_a);
 		rra(stack_a);
 	}
-	else if (head->index == 1)
+	else if (head->index == next_min)
 	{
-		if (head->next->index == 0)
+		if (head->next->index == min)
 			sa(stack_a);
 		else
 			rra(stack_a);
 	}
 	else
 	{
-		if (head->next->index == 0)
+		if (head->next->index == min)
 			ra(stack_a);
 		else
+		{
+			sa(stack_a);
 			rra(stack_a);
+		}
 	}
 }
 
@@ -47,7 +71,7 @@ static void	sort_4(t_list **stack_a, t_list **stack_b)
 
 	if (is_sorted(stack_a))
 		return ;
-	distance = get_distance(stack_a, 0);
+	distance = get_distance(stack_a, get_min(stack_a, -1));
 	if (distance == 1)
 		ra(stack_a);
 	else if (distance == 2)
@@ -60,7 +84,6 @@ static void	sort_4(t_list **stack_a, t_list **stack_b)
 	if (is_sorted(stack_a))
 		return ;
 	pb(stack_a, stack_b);
-	decrement_index(stack_a);
 	sort_3(stack_a);
 	pa(stack_a, stack_b);
 }
@@ -87,7 +110,6 @@ static void	sort_5(t_list **stack_a, t_list **stack_b)
 	if (is_sorted(stack_a))
 		return ;
 	pb(stack_a, stack_b);
-	decrement_index(stack_a);
 	sort_4(stack_a, stack_b);
 	pa(stack_a, stack_b);
 }
