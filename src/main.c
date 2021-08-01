95ae900485cc32c2a723d87410c8d9ee1e422923
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-01 11:38:50 -0400

    changed initStack function to handele the argv with quotes

--- src/main.c
+++ src/main.c
@@ -1,28 +1,33 @@
 #include "../includes/push_swap.h"
 
-static void	initStack(t_list **stack, char **argv)
+static void	initStack(t_list **stack, int argc, char **argv)
 {
 	t_list	*new;
+	char	**args;
 	int		i;
 
-	i = 1;
-	while (argv[i])
+	i = 0;
+	if (argc == 2)
+		args = ft_split(argv[1], ' ');
+	else
 	{
-		new = ft_lstnew(ft_atoi(argv[i]));
+		i = 1;
+		args = argv;
+	}
+	while (args[i])
+	{
+		new = ft_lstnew(ft_atoi(args[i]));
 		ft_lstadd_back(stack, new);
 		i++;
 	}
 	index_stack(stack);
+	// free args
 }
 
 static void	sort_stack(t_list **stack_a, t_list **stack_b, int argc)
 {
 	if (argc <= 6)
 		simple_sort(stack_a, stack_b);
-	else if (argc < 50)
-		sort(stack_a, stack_b);
-	else if (argc == 101)
-		cent_sort(stack_a, stack_b);
 	else
 		radix_sort(stack_a, stack_b);
 }
@@ -33,15 +38,15 @@ int	main(int argc, char **argv)
 {
 	t_list	**stack_a;
 	t_list	**stack_b;
-
+	
 	if (argc < 2)
 		return (-1);
-	ft_check_args(argv);
+	ft_check_args(argc, argv);
 	stack_a = (t_list **)malloc(sizeof(t_list));
 	stack_b = (t_list **)malloc(sizeof(t_list));
 	*stack_a = NULL;
 	*stack_b = NULL;
-	initStack(stack_a, argv);
+	initStack(stack_a, argc, argv);
 	if (is_sorted(stack_a))
 	{
 		free_stack(stack_a);
@@ -49,8 +54,8 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	sort_stack(stack_a, stack_b, argc);
-	if (is_sorted(stack_a))
-	ft_putendl_fd("yay", 1);
+	//if (is_sorted(stack_a))
+	//	ft_putendl_fd("yay", 1);
 	free_stack(stack_a);
 	free_stack(stack_b);
 	return (0);
